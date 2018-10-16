45596f302889c4619706bfd465ef5cd6c8db081c
Zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-10-16 11:14:45 +0800

    add code

--- lobuilder/image/build.py
+++ lobuilder/image/build.py
@@ -2,9 +2,13 @@
 # -*- coding:utf-8 -*-
 from __future__ import print_function
 
+import datetime
+import logging
 import os
 import sys
-import logging
+import shutil
+import time
+import tempfile
 from oslo_config import cfg
 from lobuilder import exception
 from lobuilder.common import config as common_config
@@ -66,20 +70,17 @@ class Worker(object):
         self.maintainer = conf.maintainer
 
     def _get_images_dir(self):
-        possible_paths = [
+        possible_paths = (
             PROJECT_ROOT,
-            os.path.join(sys.prefix, 'share/lobuilder'),
-            os.path.join(sys.prefix, 'local/share/lobuilder')]
-        if self.conf.extend_path:
-            possible_paths.index(0, self.conf.extend_path)
+            os.path.join(sys.prefix, 'share/kolla'),
+            os.path.join(sys.prefix, 'local/share/kolla'))
 
         for path in possible_paths:
             image_path = os.path.join(path, 'docker')
             # NOTE(SamYaple): We explicty check for the base folder to ensure
             #                 this is the correct path
             # TODO(SamYaple): Improve this to make this safer
-            # if os.path.exists(os.path.join(image_path, 'base')):
-            if os.path.exists(image_path):
+            if os.path.exists(os.path.join(image_path, 'base')):
                 LOG.info('Found the docker image folder at %s', image_path)
                 return image_path
         else:
@@ -141,6 +142,29 @@ class Worker(object):
             rpm_setup.append(cmd)
         return rpm_setup
 
+    def copy_apt_files(self):
+        if self.conf.apt_sources_list:
+            shutil.copyfile(
+                self.conf.apt_sources_list,
+                os.path.join(self.working_dir, "base", "sources.list")
+            )
+
+        if self.conf.apt_preferences:
+            shutil.copyfile(
+                self.conf.apt_preferences,
+                os.path.join(self.working_dir, "base", "apt_preferences")
+            )
+
+    def setup_working_dir(self):
+        """Creates a working directory for use while building"""
+        ts = time.time()
+        ts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S_')
+        self.temp_dir = tempfile.mkdtemp(prefix='lobuilder-' + ts)
+        self.working_dir = os.path.join(self.temp_dir, 'docker')
+        shutil.copytree(self.images_dir, self.working_dir)
+        self.copy_apt_files()
+        LOG.debug('Created working dir: %s', self.working_dir)
+
 
 def run_build():
     """Build container images.
@@ -153,3 +177,6 @@ def run_build():
 
     if conf.debug:
         LOG.setLevel(logging.DEBUG)
+
+    wk = Worker(conf)
+    wk.setup_working_dir()
