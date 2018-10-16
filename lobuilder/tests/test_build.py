174cbb8286ee6c7be2d6c69ed66d72380c4cb8b0
Zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-10-16 14:16:10 +0800

    fix code

--- lobuilder/tests/test_build.py
+++ lobuilder/tests/test_build.py
@@ -59,7 +59,9 @@ class WorkerTest(base.TestCase):
         self.assertEqual(2, len(wk.rpm_setup))
 
     def test_extend_docker_path(self):
+        import pdb;
+        pdb.set_trace()
         self.conf.set_default("extend_docker_path", "/fake/extend_path")
         wk = build.Worker(self.conf)
-        import pdb;pdb.set_trace()
+
         wk.setup_working_dir()
