089bd5f4c8435b44e0e9eb7e6fbe0d88846cce15
Zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-10-16 21:13:16 +0800

    fix code

---
+++ doc/source/conf.py
@@ -0,0 +1,77 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+"""
+# Created By: Zhennan.luo(Jenner)
+"""
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../..'))
+# -- General configuration ----------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'oslosphinx'
+]
+
+
+# autodoc generation is a bit aggressive and a nuisance when doing heavy
+# text edit cycles.
+# execute "export SPHINX_DEBUG=1" in your terminal to disable
+
+# The suffix of source filenames.
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = u'lobuilder'
+copyright = u'lobuilder'
+
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+from lobuilder.version import version_info as lo_version
+# The full version, including alpha/beta/rc tags.
+release = lo_version.version_string_with_vcs()
+# The short X.Y version.
+version = lo_version.canonical_version_string()
+
+# If true, '()' will be appended to :func: etc. cross-reference text.
+add_function_parentheses = True
+
+# If true, the current module name will be prepended to all description
+# unit titles (such as .. function::).
+add_module_names = True
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = 'sphinx'
+
+
+# -- Options for HTML output --------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  Major themes that come with
+# Sphinx are currently 'default' and 'sphinxdoc'.
+# html_theme_path = ["."]
+# html_theme = '_theme'
+# html_static_path = ['static']
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = '%sdoc' % project
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass
+# [howto/manual]).
+latex_documents = [
+    ('index',
+     '%s.tex' % project,
+     u'%s Documentation' % project,
+     u'Python Build Docker Images', 'manual'),
+]
---
+++ doc/source/index.rst
@@ -0,0 +1,19 @@
+..
+      Copyright 2018 lobuilder
+      All Rights Reserved.
+
+=====================================
+Welcome to lobuilder's documentation!
+=====================================
+
+lobuilder is to provide production-ready containers from Dockefile. This is
+extend from Kolla.
+
+
+Administrator Guide
+===================
+
+.. toctree::
+   :maxdepth: 2
+
+   admin/index
