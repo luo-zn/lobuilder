5e249dcfedade12ffe14782e6ee4da0075066737
Zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-10-16 20:45:42 +0800

    fix code

--- lobuilder/__init__.py
+++ lobuilder/__init__.py
@@ -1,2 +1,12 @@
 #!/bin/env python
 # -*- coding:utf-8 -*-
+
+import threading
+
+import pbr.version
+
+
+__version__ = pbr.version.VersionInfo('lobuilder').version_string()
+
+# Make a project global TLS trace storage repository
+TLS = threading.local()
---
+++ lobuilder/opts.py
@@ -0,0 +1,10 @@
+#!/bin/env python
+# -*- coding:utf-8 -*-
+"""
+# Created By: Zhennan.luo(Jenner)
+"""
+from lobuilder.common import config
+
+
+def list_opts():
+    return config.list_opts()
