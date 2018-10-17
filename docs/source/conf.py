2e645a0c2821baa3702b67d036e8c72e71a548b3
Zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-10-17 19:45:22 +0800

    Fix code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'oslosphinx'
+    # 'oslosphinx'
 ]
 
 
@@ -33,16 +33,16 @@ project = u'lobuilder'
 copyright = u'lobuilder'
 
 
-# # The version info for the project you're documenting, acts as replacement for
-# # |version| and |release|, also used in various other places throughout the
-# # built documents.
-# #
-# # The short X.Y version.
-# from lobuilder.version import version_info as lo_version
-# # The full version, including alpha/beta/rc tags.
-# release = lo_version.version_string_with_vcs()
-# # The short X.Y version.
-# version = lo_version.canonical_version_string()
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+from lobuilder.version import version_info as lo_version
+# The full version, including alpha/beta/rc tags.
+release = lo_version.version_string_with_vcs()
+# The short X.Y version.
+version = lo_version.canonical_version_string()
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = True
