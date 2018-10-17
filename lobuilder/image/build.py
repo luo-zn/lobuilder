f741ce61ea9796a28655a640be97d57fe7337202
Zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-10-17 15:55:18 +0800

    fix code

--- lobuilder/image/build.py
+++ lobuilder/image/build.py
@@ -23,7 +23,13 @@ import git
 from oslo_config import cfg
 from distutils.version import StrictVersion
 from requests import exceptions as requests_exc
-from lobuilder.cmd.build import PROJECT_ROOT
+# NOTE(SamYaple): Update the search path to prefer PROJECT_ROOT as the source
+#                 of packages to import if we are using local tools instead of
+#                 pip installed
+PROJECT_ROOT = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), '../..'))
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 from lobuilder import exception
 from lobuilder.common import task
 from lobuilder.common import config as common_config
